6ae23e41168440377fd532df6e6ac16765aa8da2
Adrià Mercader <amercadero@gmail.com>
2011-04-11 16:53:47 +0100

    Initial commit. This extension will hold geo-related functionalities moved from ckanext-harvest.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-spatial',
+	version=version,
+	description="Geo-related plugins for CKAN",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Open Knowledge Foundation',
+	author_email='info@okfn.org',
+	url='http://okfn.org',
+	license='AGPL',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.spatial'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.spatial:PluginClass
+	""",
+)
