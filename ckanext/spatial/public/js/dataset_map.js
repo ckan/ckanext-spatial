70f7f6144b4e324384ae01abe0a23ed14b125741
amercader <amercadero@gmail.com>
2013-01-18 13:32:30 +0000

    Factor out DGU code from the dataset map
    It now lives in the DGU extension:
    
    https://github.com/datagovuk/ckanext-dgu/blob/master/ckanext/dgu/theme/public/scripts/dgu-dataset-map.js

--- ckanext/spatial/public/js/dataset_map.js
+++ ckanext/spatial/public/js/dataset_map.js
@@ -37,8 +37,6 @@ this.ckan.module('dataset-map', function (jQuery, _) {
       if (!this.extent)
           return false;
 
-      this.map_type = this.map_type || "osm";
-
       // Setup some sizes
       var width = $(this.el).width();
       if (width > 1024) {
@@ -50,76 +48,34 @@ this.ckan.module('dataset-map', function (jQuery, _) {
 
       var showZoomControls = (width > 300);
 
-      if (this.map_type=='osm') {
-          var mapquestTiles = [
-              "http://otile1.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
-              "http://otile2.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
-              "http://otile3.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
-              "http://otile4.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg"];
+      var mapquestTiles = [
+          "http://otile1.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
+          "http://otile2.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
+          "http://otile3.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
+          "http://otile4.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg"];
 
-          var layers = [
-            new OpenLayers.Layer.OSM("MapQuest-OSM Tiles", mapquestTiles)
-          ];
+      var layers = [
+        new OpenLayers.Layer.OSM("MapQuest-OSM Tiles", mapquestTiles)
+      ];
 
-          // Create a new map
-          this.map = new OpenLayers.Map("dataset-map-container" ,
-              {
-              "projection": new OpenLayers.Projection("EPSG:900913"),
-              "displayProjection": new OpenLayers.Projection("EPSG:4326"),
-              "units": "m",
-              "numZoomLevels": 18,
-              "maxResolution": 156543.0339,
-              "maxExtent": new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34),
-              "controls": [
-                  new OpenLayers.Control.Navigation()
-              ],
-              "theme":"/js/vendor/openlayers/theme/default/style.css"
-          });
-          if (showZoomControls) this.map.addControl(new OpenLayers.Control.PanZoom());
-          var internalProjection = new OpenLayers.Projection("EPSG:900913");
-      } else if (this.map_type=='os') {
+      // Create a new map
+      this.map = new OpenLayers.Map("dataset-map-container" ,
+          {
+          "projection": new OpenLayers.Projection("EPSG:900913"),
+          "displayProjection": new OpenLayers.Projection("EPSG:4326"),
+          "units": "m",
+          "numZoomLevels": 18,
+          "maxResolution": 156543.0339,
+          "maxExtent": new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34),
+          "controls": [
+              new OpenLayers.Control.Navigation()
+          ],
+          "theme":"/js/vendor/openlayers/theme/default/style.css"
+      });
 
-          var copyrightStatements = "Contains Ordnance Survey data (c) Crown copyright and database right  [2012] <br>" + "Contains Royal Mail data (c) Royal Mail copyright and database right [2012]<br>" + "Contains bathymetry data by GEBCO (c) Copyright [2012]<br>" + "Contains data by Land & Property Services (Northern Ireland) (c) Crown copyright [2012]";
+      if (showZoomControls) this.map.addControl(new OpenLayers.Control.PanZoom());
+      var internalProjection = new OpenLayers.Projection("EPSG:900913");
 
-          // Create a new map
-          var layers = [
-            new OpenLayers.Layer.WMS("Geoserver layers - Tiled",
-'http://osinspiremappingprod.ordnancesurvey.co.uk/geoserver/gwc/service/wms', {
-                  LAYERS: 'InspireETRS89',
-      STYLES: '',
-      format: 'image/png',
-      tiled: true
-                  }, {
-      buffer: 0,
-      displayOutsideMaxExtent: true,
-      isBaseLayer: true,
-      attribution: copyrightStatements,
-                  transitionEffect: 'resize'
-                  }
-            )
-          ];
-          OpenLayers.DOTS_PER_INCH = 90.71428571428572;
-
-    var options = {
-          size: new OpenLayers.Size(width, height),
-                            scales: [15000000, 10000000, 5000000, 1000000, 250000, 75000],
-          maxExtent: new OpenLayers.Bounds(-30, 48.00, 3.50, 64.00),
-          restrictedExtent: new OpenLayers.Bounds(-30, 48.00, 3.50, 64.00),
-          tileSize: new OpenLayers.Size(250, 250),
-          units: 'degrees',
-          projection: "EPSG:4258"
-    };
-
-          this.map = new OpenLayers.Map("dataset-map-container", {});
-
-
-
-
-    // Set the options on the map
-this.map.setOptions(options);
-
-          var internalProjection = new OpenLayers.Projection("EPSG:4258");
-      }
       this.map.addLayers(layers);
 
       var getGeomType = function(feature){
