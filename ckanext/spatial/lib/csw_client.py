fc4c37c4c31b005853c59e2880a1a82ed9035435
Tom Kralidis <tomkralidis@gmail.com>
2016-01-13 12:49:11 -0500

    add super to init

--- ckanext/spatial/lib/csw_client.py
+++ ckanext/spatial/lib/csw_client.py
@@ -68,6 +68,7 @@ class CswService(OwsService):
     from owslib.csw import CatalogueServiceWeb as _Implementation
 
     def __init__(self):
+        super(CswService, self).__init__()
         self.sortby = SortBy([SortProperty('dc:identifier')])
 
     def getrecords(self, qtype=None, keywords=[],
