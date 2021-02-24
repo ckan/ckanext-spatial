035faabff3b4717b7ccb1c18dc6f4f716b81c625
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-02-24 17:40:25 +0000

    Fix sort of imports in harvesters/base.py

--- ckanext/spatial/harvesters/base.py
+++ ckanext/spatial/harvesters/base.py
@@ -24,8 +24,8 @@ from ckan import plugins as p
 from ckan import model
 from ckan.lib.helpers import json
 from ckan import logic
-from ckan.lib.navl.validators import not_empty
 from ckan.lib.navl.dictization_functions import validate
+from ckan.lib.navl.validators import not_empty
 from ckan.lib.search.index import PackageSearchIndex
 from ckanext.harvest.harvesters.base import munge_tag
 
