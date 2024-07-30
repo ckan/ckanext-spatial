25a7266ec43ef2d55d4185af17d71af4f2469ada
Peter Vorman <pvorman@opengov.com>
2024-07-31 00:27:20 +0300

    fixed bug

--- ckanext/spatial/harvested_metadata_fgdc.py
+++ ckanext/spatial/harvested_metadata_fgdc.py
@@ -41,7 +41,7 @@ class MappedXmlDocument(MappedXmlObject):
     def get_xml_tree(self):
         if self.xml_tree is None:
             parser = etree.XMLParser(remove_blank_text=True)
-            xml_str = self.xml_str.decode(encoding='utf-8')
+            xml_str = str(self.xml_str)
             self.xml_tree = etree.fromstring(xml_str, parser=parser)
         return self.xml_tree
 
